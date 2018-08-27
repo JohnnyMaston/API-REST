84b4e62848c57ddb373547b7ca8650b6158eff0f
Juan Jose Herrera Saldaña <jujohersamaster@hotmail.com>
2018-08-27 18:54:01 -0500

    Docker
    Si haces una imagen con mi codigo, dame el credito por favor.

--- socket/usockets.js
+++ socket/usockets.js
@@ -1,4 +1,6 @@
 //sockets para el envio de las notificaciones de los usuarios
+//para mantener la conexion de punto a punto entre el servidor y el frontend 
+//en proceso
 function usockets() {
 var Socket = new WebSocket("http://localhost:3000");
 
